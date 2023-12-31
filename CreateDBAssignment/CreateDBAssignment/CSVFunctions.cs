eda85af989067e2d9e0eb8e28b864e741311e71b
ShoshanaBezalel <91996753+ShoshanaBezalel@users.noreply.github.com>
2023-12-31 15:18:30 +0200

    first commit

---
+++ CreateDBAssignment/CreateDBAssignment/CSVFunctions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace CreateDBAssignment
+{
+    internal class CSVFunctions
+    {
+        public static void GenerateCSVFile(string filePath, int rowCount)
+        {
+            Random random = new Random();
+            List<string> lines = new List<string> { "FirstName, LastName, Age, Weight, Gender" };
+
+            for (int i = 0; i < rowCount; i++)
+            {
+                string firstName = GetRadnomFName();
+                string lastName = GetRadnomLName();
+                int age = random.Next(18, 71);
+                double weight = random.Next(100, 300) / 2.20462;
+                string gender = random.Next(2) == 0 ? "Male" : "Female";
+
+                lines.Add($"{firstName},{lastName},{age},{weight:F2},{gender}");
+            }
+
+            File.WriteAllLines(filePath, lines);
+        }
+
+        public static List<Person> ReadCSVFile(string filePath)
+        {            var peopleData = new List<Person>();
+
+            var lines = File.ReadLines(filePath).Skip(1);
+
+            foreach (var line in lines)
+            {
+                var values = line.Split(',');
+                var person = new Person
+                {
+                    FirstName = values[0],
+                    LastName = values[1],
+                    Age = int.Parse(values[2]),
+                    Weight = double.Parse(values[3], CultureInfo.InvariantCulture),
+                    Gender = values[4],
+                };
+
+                peopleData.Add(person);
+            }
+
+            return peopleData;
+        }
+
+        private static string GetRadnomLName()
+        {
+            string[] firstNames = { "John", "Jane", "Michael", "Emily", "David", "Emma", "Christopher", "Olivia", "Daniel", "Sophia" };
+            return firstNames[new Random().Next(firstNames.Length)];
+        }
+
+        private static string GetRadnomFName()
+        {
+            string[] lastNames = { "Smith", "Johnson", "Williams", "Brown", "Jones", "Miller", "Davis", "García", "Rodriguez", "Martinez" };
+            return lastNames[new Random().Next(lastNames.Length)];
+        }
+    }
+
+}
+
---
+++ CreateDBAssignment/CreateDBAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using CreateDBAssignment;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    static void Main()
+    {
+        string csvFilePath = "people_data.csv";
+        CSVFunctions.GenerateCSVFile(csvFilePath, 1000);
+
+        var peopleData = CSVFunctions.ReadCSVFile(csvFilePath);
+
+        double averageAge = peopleData.Select(p => p.Age).Average();
+        Console.WriteLine($"Average age of all people: {averageAge:F2} years.");
+
+        int peopleInWeightRange = peopleData.Count(p => p.Weight > 120 && p.Weight <= 140);
+        Console.WriteLine($"Total Number of People Weighing between 120lbs and 140lbs: {peopleInWeightRange}");
+
+        double averageAgeInWeightRange = peopleData
+            .Where(p => p.Weight >= 120 && p.Weight <= 140)
+            .Select(p => p.Weight)
+            .Average();
+
+        Console.WriteLine($"Average Age of People Weighing between 120lbs and 140lbs: {averageAgeInWeightRange:F2} years");
+
+    }
+}
+
+
+
